5f2a98cf1c75228bae2262ba423320c08b2ac879
Pechlivanidis <6hk-m3f-u1@fh-h.de>
2023-05-17 17:26:58 +0200

    Rebase Update 1

--- Main.java
+++ Main.java
@@ -29,7 +29,7 @@ public class Main
         }
         
         System.out.println("Summe: " + summe + " Mult: " + mult);
-        //Master Update 1
+        //Rebase Update 1
         //Master Update 2
     }
 }
